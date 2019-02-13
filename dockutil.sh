361044ea737667dbaa0139f2c988674283bca5d2
Komil Vokhidov <kvl8565@stu-kvl8565-18.qatar.private>
2019-02-13 16:20:35 +0300

    the best code ever made

--- dockutil.sh
+++ dockutil.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
 
-# Putting Steam on dock station
-/usr/local/Cellar/dockutil/2.0.5/bin/dockutil --add /Applications/Steam.app
-# Putting Quicktime on dock station
-/usr/local/Cellar/dockutil/2.0.5/bin/dockutil --add /Applications/QuickTime\ Player.app
-# Putting Self Service on dock station
+# Putting Self Serfice
 /usr/local/Cellar/dockutil/2.0.5/bin/dockutil --add /Applications/Self\ Service.app
-# Remove Photoshop from Dock station
-dockutil --remove /Applications/Adobe\ Photoshop\ CC\ 2018/Adobe\ Photoshop\ CC\ 2018.app
+
+# Putting Adobe Photoshop 2018
+/usr/local/Cellar/dockutil/2.0.5/bin/dockutil --add /Applications/Adobe\ Photoshop\ CC\ 2018/Adobe\ Photoshop\ CC\ 2018.app
+
+# Putting Outlook 
+/usr/local/Cellar/dockutil/2.0.5/bin/dockutil --add /Applications/Microsoft\ Outlook.app
+
+# Putting Microsoft Word 
+/usr/local/Cellar/dockutil/2.0.5/bin/dockutil --add /Applications/Microsoft\ Word.app
+
+# Putting Microsoft Excel 
+/usr/local/Cellar/dockutil/2.0.5/bin/dockutil --add /Applications/Microsoft\ Excel.app
+
+#Putting Microsoft Power Point
+/usr/local/Cellar/dockutil/2.0.5/bin/dockutil --add /Applications/Microsoft\ PowerPoint.app
+
+# Putting Adone Premiere Pro 2018
+/usr/local/Cellar/dockutil/2.0.5/bin/dockutil --add /Applications/Adobe\ Premiere\ Pro\ CC\ 2018/Adobe\ Premiere\ Pro\ CC\ 2018.app
+
+# Remove Photoshop 2017 from Dock station
+/usr/local/Cellar/dockutil/2.0.5/bin/dockutil --remove 'Adobe Photoshop CC 2017'
+
+# Remove Premiere Pro 2017 from Dock station
+/usr/local/Cellar/dockutil/2.0.5/bin/dockutil --remove 'Adobe Premiere Pro CC 2017'
+
+
 
