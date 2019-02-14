4f648fae5a257cf08b3f00cb5858e0f6f7812f4e
Komil Vokhidov <kvl8565@stu-kvl8565-18.qatar.private>
2019-02-14 11:26:19 +0300

    changes

--- dockutil.sh
+++ dockutil.sh
@@ -28,5 +28,11 @@
 # Remove Premiere Pro 2017 from Dock station
 /usr/local/Cellar/dockutil/2.0.5/bin/dockutil --remove 'Adobe Premiere Pro CC 2017'
 
+# Remove Maps from Dock station
+/usr/local/Cellar/dockutil/2.0.5/bin/dockutil --remove 'Maps'
+
+# Remove Photos from Dock station
+/usr/local/Cellar/dockutil/2.0.5/bin/dockutil --remove 'Photos'
+
 
 
